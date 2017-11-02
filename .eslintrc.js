374e089aa99e2984ce8d113416929bf7a8539742
xue453570847 <453570847@qq.com>
2017-11-02 20:35:25 +0800

    dev

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,9 @@ module.exports = {
   settings: {
     'html/html-extensions': ['.html', '.wpy']
   },
+  "globals": {
+    "wx": true
+  },
   // add your custom rules here
   'rules': {
     // allow paren-less arrow functions
